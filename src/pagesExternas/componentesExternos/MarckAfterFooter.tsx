7d4bf85aedf0eff73c761c8408ce480221fdaa9b
Ryan <ryandsferrer@gmail.com>
2024-12-09 23:55:13 -0300

    feat: finalization of the contact page with some style adjustments missing

---
+++ src/pagesExternas/componentesExternos/MarckAfterFooter.tsx
@@ -0,0 +1,7 @@
+
+
+const MarckComponent= ()=>{
+    return(
+        
+    )
+}
--- src/pagesExternas/contact/contact.tsx
+++ src/pagesExternas/contact/contact.tsx
@@ -1,6 +1,13 @@
 import { FaLocationPin, FaPhone, FaClock } from "react-icons/fa6"
 import ImgContact from "./imgContact/ShopBanner.png"
 
+
+const styleTitlleIten= 'font-semibold text-[19px]'
+const StyleItensInform= 'flex flex-row gap-6'
+const stylelabel= 'w-[190px]'
+const styleInput= 'h-[52px] w-[450px] border border-[#9F9F9F] rounded-[5px]'
+const StylelabelInputs= 'h-[100px] flex flex-col justify-between'
+const StyleTitlleInput= 'text-[16px] font-semibold'
 const Contact= ()=>{
     return(
         <section>
@@ -15,66 +22,67 @@ const Contact= ()=>{
                     <p>Contact</p>
                 </div>
             </div>
-            <div>
-                <div>
-                    <h2>Get In Touch With Us</h2>
-                    <p>For More Information About Our Product & Services.
+            <div className="h-[950px] flex flex-col items-center pt-20 px-[300px] gap-[100px] justify-between">
+                <div className="w-[580px] h-[100px] justify-between flex flex-col items-center">
+                    <h2 className="text-[30px] font-semibold">Get In Touch With Us</h2>
+                    <p className="text-center text-[#9F9F9F]">For More Information About Our Product & Services.
                          Please Feel Free To Drop Us An Email. Our Staff
                           Always Be There To Help You Out. Do Not Hesitate!</p>
                 </div>
-                <div>
-                    <ul>
-                        <li>
-                            <FaLocationPin />
-                            <label>
-                                <p>Address</p>
+                <div className="flex flex-row h-full w-full justify-between">
+                    <ul className="flex flex-col h-[370px] justify-between">
+                        <li className={StyleItensInform}>
+                            <span className="mt-0"><FaLocationPin /></span>
+                            <label className={stylelabel}>
+                                <p className={styleTitlleIten}>Address</p>
                                 <p>236 5th SE Avenue, New York NY10000, United States</p>
                             </label>
                         </li>
-                        <li>
-                            <FaPhone />
-                            <label>
-                                <p>Phone</p>
+                        <li className={StyleItensInform}>
+                            <span className="mt-[7px]"><FaPhone /></span>
+                            <label className={stylelabel}>
+                                <p className={styleTitlleIten}>Phone</p>
                                 <p>Mobile: +(84) 546-6789
                                    Hotline: +(84) 456-6789</p>
                             </label>
                         </li>
-                        <li>
-                            <FaClock />
-                            <label>
+                        <li className={StyleItensInform}>
+                            <span className="mt-2"><FaClock /></span>
+                            <label className={stylelabel}>
+                                <p className={styleTitlleIten}>Working Time</p>
                                 <p>Monday-Friday: 9:00 - 22:00
-                                   Saturday-Sunday: 9:00 - 21:00</p>
+                                   <br/>Saturday-Sunday: 9:00 - 21:00</p>
                             </label>
                         </li>
                     </ul>
-                    <div>
-                        <ul>
+                    <div className="h-[620px] flex flex-col justify-between">
+                        <ul className="flex flex-col justify-between h-[520px]">
                             <li>
-                                <label>
-                                    <p>Your name</p>
-                                    <input type="texte" />
+                                <label className={StylelabelInputs}>
+                                    <p className={StyleTitlleInput}>Your name</p>
+                                    <input className={styleInput} type="texte" />
                                 </label>
                             </li>
                             <li>
-                                <label>
-                                    <p>Your name</p>
-                                    <input type="texte" />
+                                <label className={StylelabelInputs}>
+                                    <p className={StyleTitlleInput}>Email address</p>
+                                    <input className={styleInput} type="texte" />
                                 </label>
                             </li>
                             <li>
-                                <label>
-                                    <p>Your name</p>
-                                    <input type="texte" />
+                                <label className={StylelabelInputs}>
+                                    <p className={StyleTitlleInput}>Subject</p>
+                                    <input className={styleInput} type="texte" />
                                 </label>
                             </li>
                             <li>
-                                <label>
-                                    <p>Your name</p>
-                                    <input type="texte" />
+                                <label className="h-[145px] flex flex-col justify-between">
+                                    <p className={StyleTitlleInput}>Message</p>
+                                    <input className="h-[100px] w-[450px] border border-[#9F9F9F] rounded-[5px]" type="texte" />
                                 </label>
                             </li>
                         </ul>
-                        <button>Submit</button>
+                        <button className="bg-[#B88E2F] text-white w-[237px] h-[55px] rounded-[5px]">Submit</button>
                     </div>
                 </div>
             </div>
